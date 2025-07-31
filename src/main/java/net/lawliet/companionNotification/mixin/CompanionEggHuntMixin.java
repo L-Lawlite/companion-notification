bee006b2c91b030d93a54a9207efba55a3fb6935
Lawliet <junirobert21@gmail.com>
2025-07-31 17:18:55 +0530

    Fixed discount = 1 bug

--- src/main/java/net/lawliet/companionNotification/mixin/CompanionEggHuntMixin.java
+++ src/main/java/net/lawliet/companionNotification/mixin/CompanionEggHuntMixin.java
@@ -2,19 +2,21 @@ package net.lawliet.companionNotification.mixin;
 
 import com.llamalad7.mixinextras.expression.Definition;
 import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import iskallia.vault.core.data.key.FieldKey;
 import iskallia.vault.core.vault.Vault;
 import iskallia.vault.core.vault.companion.CompanionEggHunt;
+import iskallia.vault.core.vault.companion.HuntInstance;
 import net.minecraft.ChatFormatting;
 import net.minecraft.Util;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.server.level.ServerPlayer;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Debug(export = true)
 @Mixin(value = CompanionEggHunt.class, remap = false)
 public class CompanionEggHuntMixin {
 
@@ -24,5 +26,14 @@ public class CompanionEggHuntMixin {
     @Inject(method = "createHunt", at = @At(value = "MIXINEXTRAS:EXPRESSION", shift = At.Shift.AFTER))
     public void addCompanionNotification(ServerPlayer player, Vault vault, CallbackInfo ci) {
         player.sendMessage((new TextComponent("Companion is watching you!").withStyle(ChatFormatting.GREEN)), Util.NIL_UUID);
+    }
+
+    @Definition(id = "THRESHOLD", field = "Liskallia/vault/core/vault/companion/HuntInstance;THRESHOLD:Liskallia/vault/core/data/key/FieldKey;")
+    @Definition(id = "get", method = "Liskallia/vault/core/vault/companion/HuntInstance;get(Liskallia/vault/core/data/key/FieldKey;)Ljava/lang/Object;")
+    @Expression("?.get(THRESHOLD)")
+    @WrapOperation(method = "tickServer", at = @At(value = "MIXINEXTRAS:EXPRESSION", ordinal = 0))
+    public Object thresholdCondition(HuntInstance instance, FieldKey fieldKey, Operation<Object> original) {
+
+            return  instance.get(fieldKey).equals(0) ? 1 : original;
     }
 }
