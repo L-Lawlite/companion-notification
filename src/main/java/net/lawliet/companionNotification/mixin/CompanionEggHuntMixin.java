b3538e56d17b56edf289f705682441cad331bcbc
Lawliet <junirobert21@gmail.com>
2025-08-01 16:08:58 +0530

    19.5 bug fix removed as it is not needed

--- src/main/java/net/lawliet/companionNotification/mixin/CompanionEggHuntMixin.java
+++ src/main/java/net/lawliet/companionNotification/mixin/CompanionEggHuntMixin.java
@@ -2,12 +2,8 @@ package net.lawliet.companionNotification.mixin;
 
 import com.llamalad7.mixinextras.expression.Definition;
 import com.llamalad7.mixinextras.expression.Expression;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import iskallia.vault.core.data.key.FieldKey;
 import iskallia.vault.core.vault.Vault;
 import iskallia.vault.core.vault.companion.CompanionEggHunt;
-import iskallia.vault.core.vault.companion.HuntInstance;
 import net.minecraft.ChatFormatting;
 import net.minecraft.Util;
 import net.minecraft.network.chat.TextComponent;
@@ -26,13 +22,5 @@ public class CompanionEggHuntMixin {
     @Inject(method = "createHunt", at = @At(value = "MIXINEXTRAS:EXPRESSION", shift = At.Shift.AFTER))
     public void addCompanionNotification(ServerPlayer player, Vault vault, CallbackInfo ci) {
         player.sendMessage((new TextComponent("Companion is watching you!").withStyle(ChatFormatting.GREEN)), Util.NIL_UUID);
-    }
-
-    @Definition(id = "THRESHOLD", field = "Liskallia/vault/core/vault/companion/HuntInstance;THRESHOLD:Liskallia/vault/core/data/key/FieldKey;")
-    @Definition(id = "get", method = "Liskallia/vault/core/vault/companion/HuntInstance;get(Liskallia/vault/core/data/key/FieldKey;)Ljava/lang/Object;")
-    @Expression("?.get(THRESHOLD)")
-    @WrapOperation(method = "tickServer", at = @At(value = "MIXINEXTRAS:EXPRESSION", ordinal = 0))
-    public Object thresholdCondition(HuntInstance instance, FieldKey fieldKey, Operation<Object> original) {
-            return  instance.get(fieldKey).equals(0) ? 1 : instance.get(fieldKey);
     }
 }
